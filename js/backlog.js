232c3c033021a437301b5b607ce7e91bf0512067
Herczku Misi <kmisku@gmail.com>
2016-05-31 20:21:23 +0200

    Add Backlog items to the table
    not perfect yet, and not use of local storage

--- js/backlog.js
+++ js/backlog.js
@@ -1,3 +1,36 @@
 /**
  * Created by herczkumihalybalazs on 2016.05.31..
  */
+
+document.getElementById("addItemButton").addEventListener("click", addItemToTable)
+
+function gennerateId() {
+
+}
+
+function addItemToTable() {
+
+    var itemId = gennerateId();
+    var itemName = document.getElementById("itemName").value;
+    var itemStatus = document.getElementById("itemStatus").value;
+    var itemEffort = document.getElementById("itemEffort").value;
+    var itemPriority = document.getElementById("itemPriority").value;
+
+    var table = document.getElementById("itemTable");
+    var row = table.insertRow(1);
+
+    var idCell = row.insertCell(0);
+    var itemNameCell = row.insertCell(1);
+    var itemStatusCell = row.insertCell(2);
+    var itemEffortCell = row.insertCell(3);
+    var itemPriorityCell = row.insertCell(4);
+
+
+
+    idCell.innerHTML = itemId;
+    itemNameCell.innerHTML = itemName;
+    itemStatusCell.innerHTML = itemStatus;
+    itemEffortCell.innerHTML = itemEffort;
+    itemPriorityCell.innerHTML = itemPriority;
+
+}
