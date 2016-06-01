abd4a6b7da466498074fb820b3855f4fd9634637
Herczku Misi <kmisku@gmail.com>
2016-06-01 14:14:15 +0200

    Add basic LocalStorage logic to backlog

--- js/backlog.js
+++ js/backlog.js
@@ -2,27 +2,24 @@
  * Created by herczkumihalybalazs on 2016.05.31..
  */
 
-document.getElementById("addItemButton").addEventListener("click", addItemToTable)
+document.getElementById("addItemButton").addEventListener("click", addItemToLocalStorage);
 
 function gennerateId() {
-var uniq = 'id' + (new Date()).getTime().toString();
+    var uniq = 'id' + (new Date()).getTime().toString();
 }
 
 
-var titleLenght = function (text)
-{
-  if (text.length <= 50)
-            {
-                return true;
-            }
-            else
-            {
-              alert("This title very long! Title max lenght 50 characters ")
-              return false;
-            }
+var titleLenght = function (text) {
+    if (text.length <= 50) {
+        return true;
+    }
+    else {
+        alert("This title very long! Title max lenght 50 characters ")
+        return false;
+    }
+};
 
-
-function addItemToTable() {
+function addItemToLocalStorage() {
 
     var itemId = gennerateId();
     var itemName = document.getElementById("itemName").value;
@@ -30,6 +27,19 @@ function addItemToTable() {
     var itemEffort = document.getElementById("itemEffort").value;
     var itemPriority = document.getElementById("itemPriority").value;
 
+    var itemProperties = [itemName, itemStatus, itemEffort, itemPriority];
+    localStorage.setItem("item", JSON.stringify(itemProperties));
+
+    var itemdetails = localStorage.getItem("item");
+    var parsedDetails = JSON.parse(itemdetails);
+    console.log(parsedDetails[1]);
+
+
+
+
+}
+
+function listItemsToTable() {
     var table = document.getElementById("itemTable");
     var row = table.insertRow(1);
 
@@ -40,11 +50,10 @@ function addItemToTable() {
     var itemPriorityCell = row.insertCell(4);
 
 
-
     idCell.innerHTML = itemId;
     itemNameCell.innerHTML = itemName;
     itemStatusCell.innerHTML = itemStatus;
     itemEffortCell.innerHTML = itemEffort;
     itemPriorityCell.innerHTML = itemPriority;
-
 }
+
